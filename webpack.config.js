a07e3b4af2130fb1107f319a1ef859cc9934c881
gitfilix <runlevel07@gmail.com>
2017-10-06 17:41:23 +0200

    initial setup with webpack for a lousy console.log

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: ['./app/index.js'],
+  output: {
+    path: __dirname +'/build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/, // a regular expression that catches .js files
+        exclude: /node-modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    port: 3001,
+    contentBase: './build',
+    inline: true
+  }
+}
