b3bf087260349cf380195b3a8a6cbc10d57e5a09
gitfilix <runlevel07@gmail.com>
2017-10-06 19:29:41 +0200

    search by click and keypress enter

--- app/components/Global.js
+++ app/components/Global.js
@@ -1,11 +1,46 @@
 // import Component feature from react
 import React, { Component } from 'react';
-
+// import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class Global extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+
+
+
+    search() {
+        console.log("searching: ", this.state.query);
+    }
+
     render() {
         return (
-            <div>Here The Global Component!</div>
+            <div>
+                <h2>Book explorer!</h2>
+                <FormGroup>
+                    <InputGroup>
+                        {/* searchfilld */}
+                        <FormControl
+                            type="text"
+                            placeholder="Search for a Book"
+                            onChange={event => this.setState({query: event.target.value})}
+                            onKeyPress={event => {
+                                if (event.key === "Enter") {
+                                    this.search();
+                                }
+                            }}
+                        />
+                        <InputGroup.Addon onClick={()=> this.search()}>
+                            <Glyphicon className="searchButton" glyph="search"></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+            </div>
         )
     }
 }
