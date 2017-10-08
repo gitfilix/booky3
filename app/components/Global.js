ab66d68be59bc7e86a000ca5090c3a4e35e2260a
gitfilix <runlevel07@gmail.com>
2017-10-08 14:18:41 +0200

    Fetch data and add it to state items obj

--- app/components/Global.js
+++ app/components/Global.js
@@ -2,12 +2,24 @@
 import React, { Component } from 'react';
 // import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+// import Component Gallery
+import Gallery from './Gallery.js';
+
+
+// google book api v1
+// https://developers.google.com/books/docs/v1/using
+// https://www.googleapis.com/books/v1/volumes?q
+
+
+
 
 class Global extends Component {
     constructor(props) {
         super(props);
+        // state obj is current query
         this.state = {
-            query: ''
+            query: '',
+            items: []
         }
     }
 
@@ -15,15 +27,27 @@ class Global extends Component {
 
 
     search() {
+        // query a book on google apii
+        const BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
+        // fecht with a promise
+        fetch( `${BASE_URL}${this.state.query}`, { method: 'GET' })
+            .then(response => response.json())
+            .then(json => {
+                let { items } = json;
+                this.setState({items});
+            }
+        );
+
+
         console.log("searching: ", this.state.query);
     }
 
     render() {
         return (
-            <div>
-                <h2>Book explorer!</h2>
+            <div className="global">
+                <h2 className="title">Filix Book store</h2>
                 <FormGroup>
-                    <InputGroup>
+                    <InputGroup className="searchbar">
                         {/* searchfilld */}
                         <FormControl
                             type="text"
@@ -40,6 +64,7 @@ class Global extends Component {
                         </InputGroup.Addon>
                     </InputGroup>
                 </FormGroup>
+                <Gallery />
             </div>
         )
     }
